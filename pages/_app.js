64543456cb5ccc3893d2d9aaf0c1eb2d989ff167
AyIshola <isholaolawale256@gmail.com>
2022-10-15 04:26:56 +0100

    Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import '../styles/GlobalStyle/Nav.css'
 
 
 function MyApp({ Component, pageProps }) {
-  return (<Component {...pageProps} />)
+  return (
+  <div>
+      <Component {...pageProps} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600;700&display=swap" rel="stylesheet"></link>
+  </div>)
 }
 
 export default MyApp
